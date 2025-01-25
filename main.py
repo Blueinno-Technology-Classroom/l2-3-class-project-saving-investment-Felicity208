339aea88f40709a526709830f43530bed43f1ff5
Felicity <felicity.curreem@gmail.com>
2025-01-25 12:18:16 +0800

    L3 - 3

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ Q1:
 
 # TODO: Write your code here
 
+currentBalance = 1000
+interest= 0
+deposit = 100
+newBalance = 0
+
+for i in range(1, 26):
+    interest = currentBalance * 0.065
+    newBalance = (currentBalance * 0.065) + currentBalance 
+    print(f'{i:02d}. Current balance: {currentBalance:.2f}, interest: {interest:.2f}, deposit: {deposit}, new balance: {newBalance:.2f}')
+    currentBalance = newBalance
+    currentBalance += deposit 
+    
 ##################################################
 '''
 Q2:
